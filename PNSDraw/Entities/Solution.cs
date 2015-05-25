5d7a4db371d0f1834f21f265c31dd1a734911937
kiskacsa08 <ahorvath1991@gmail.com>
2015-05-25 20:59:20 +0200

    Solution osztály a PGraph-ból

---
+++ PNSDraw/Entities/Solution.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PNSDraw.Entities
+{
+    public class Solution
+    {
+        public Dictionary<string, double> Materials;
+        public Dictionary<string, double> OperatingUnits;
+
+        public int Index = 0;
+
+        public string Title = "";
+
+        public Solution(int index, string title)
+        {
+            Materials = new Dictionary<string, double>();
+            OperatingUnits = new Dictionary<string, double>();
+            Index = index;
+            Title = title;
+        }
+
+
+        public void AddMaterial(string name, double flow)
+        {
+            if (Materials.ContainsKey(name) == false)
+            {
+                Materials[name] = flow;
+            }
+        }
+
+
+        public void RemoveMaterial(string name)
+        {
+            if (Materials.ContainsKey(name) == true)
+            {
+                Materials.Remove(name);
+            }
+        }
+
+        public void AddOperatingUnit(string name, double size)
+        {
+            if (OperatingUnits.ContainsKey(name) == false)
+            {
+                OperatingUnits[name] = size;
+            }
+        }
+
+
+        public void RemoveOperatingUnit(string name)
+        {
+            if (OperatingUnits.ContainsKey(name) == true)
+            {
+                OperatingUnits.Remove(name);
+            }
+        }
+
+    }
+
+    class Solutions : List<Solution> { }
+}
