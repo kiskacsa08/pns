35949d3863690ec10113919e2eed9c590b8a77b5
kiskacsa08 <ahorvath1991@gmail.com>
2015-05-25 20:55:51 +0200

    Solution osztályt külön fájlba raktam

--- PNSDraw/Entities/PGraph.cs
+++ PNSDraw/Entities/PGraph.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
 using System.IO;
+using PNSDraw.Entities;
 
 namespace PNSDraw
 {
@@ -1081,64 +1082,5 @@ namespace PNSDraw
             }
             
         }
-
-
-
-
-        class Solution
-        {
-            public Dictionary<string, double> Materials;
-            public Dictionary<string, double> OperatingUnits;
-
-            public int Index = 0;
-
-            public string Title = "";
-
-            public Solution(int index, string title)
-            {
-                Materials = new Dictionary<string, double>();
-                OperatingUnits = new Dictionary<string, double>();
-                Index = index;
-                Title = title;
-            }
-
-
-            public void AddMaterial(string name, double flow)
-            {
-                if (Materials.ContainsKey(name) == false)
-                {
-                    Materials[name] = flow;
-                }
-            }
-
-
-            public void RemoveMaterial(string name)
-            {
-                if (Materials.ContainsKey(name) == true)
-                {
-                    Materials.Remove(name);
-                }
-            }
-
-            public void AddOperatingUnit(string name, double size)
-            {
-                if (OperatingUnits.ContainsKey(name) == false)
-                {
-                    OperatingUnits[name] = size;
-                }
-            }
-
-
-            public void RemoveOperatingUnit(string name)
-            {
-                if (OperatingUnits.ContainsKey(name) == true)
-                {
-                    OperatingUnits.Remove(name);
-                }
-            }
-
-        }
-
-        
     }
 }
