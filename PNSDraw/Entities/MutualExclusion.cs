05c39e185fae0b6418f6b1a659b0dbc32109819d
kiskacsa08 <ahorvath1991@gmail.com>
2015-05-28 21:27:03 +0200

    MutualExclusion osztály

---
+++ PNSDraw/Entities/MutualExclusion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PNSDraw.Entities
+{
+    public class MutualExclusion
+    {
+        public string Name { get; set; }
+        public List<OperatingUnit> OpUnits { get; set; }
+        public int ID { get; set; }
+
+        public MutualExclusion(string name, List<OperatingUnit> opUnits)
+        {
+            ID = 0;
+            this.Name = name;
+            this.OpUnits = opUnits;
+        }
+
+        public MutualExclusion(List<OperatingUnit> opUnits)
+        {
+            ID = 0;
+            this.OpUnits = opUnits;
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[");
+            int i = 0;
+            foreach (OperatingUnit ou in this.OpUnits)
+            {
+                if (i < 3)
+                {
+                    sb.Append(ou.Name);
+                    sb.Append(";");
+                }
+                i++;
+            }
+            sb.Remove(sb.Length - 1, 1);
+            if (i > 3)
+            {
+                sb.Append(";...");
+            }
+            sb.Append("]");
+
+            this.Name = sb.ToString();
+            
+        }
+    }
+}
