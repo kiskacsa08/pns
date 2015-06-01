ec4610a4f34122596b78083de578a3ad37dba3fe
kiskacsa08 <ahorvath1991@gmail.com>
2015-06-01 15:03:46 +0200

    Kisebb javítások, hogy írhatóak is legyenek a tulajdonságok, ne csak olvashatóak

--- PNSDraw/Entities/Default.cs
+++ PNSDraw/Entities/Default.cs
@@ -8,25 +8,55 @@ namespace PNSDraw
 {
     class Default
     {
+        #region Enums
+        public enum MassUnit
+        {
+            g,
+            kg,
+            t
+        }
+
+        public enum TimeUnit
+        {
+            s,
+            h,
+            d,
+            w,
+            m,
+            y
+        }
+
+        public enum MoneyUnit
+        {
+            EUR,
+            HUF
+        }
+        #endregion
+
+        #region Values
         static public double flow_rate_lower_bound = 0; //flow_rate_lower_bound
         static public double flow_rate_upper_bound = 100000000; //flow_rate_upper_bound
         static public double price = 0; //price
-        static public int type { get { return Globals.MaterialTypes.Raw; } } //type
+        static public int type = Globals.MaterialTypes.Raw; //type
 
         static private double o_fix = 0; //fix_cost
         static private double i_fix = 0; //fix_cost
 
-        static public double fix_cost { get { return o_fix + i_fix; } }
+        public static double fix_cost = o_fix + i_fix;
 
         static private double o_prop = 0; //proportional_cost
         static private double i_prop = 0; //proportional_cost
 
-        static public double prop_cost { get { return o_prop + i_prop; } }
+        public static double prop_cost = o_prop + i_prop;
 
         static public double capacity_lower_bound = 0; //capacity_lower_bound
         static public double capacity_upper_bound = 100000000; //capacity_upper_bound
 
         static public double io_flowrate = 1; //flow rate
+
+        static public MassUnit mass_mu = MassUnit.t;
+        static public TimeUnit time_mu = TimeUnit.y;
+        static public MoneyUnit money_mu = MoneyUnit.EUR;
 
         public const double d_NperA = -1;
         static public double payout_period = 10;
@@ -36,6 +66,7 @@ namespace PNSDraw
         static public double d_solution_tolerance = 0.0001;
         static public int max_solutions = 10;
         static public bool b_auto_convert = false;
+        #endregion
 
         public Default() { }
     }
