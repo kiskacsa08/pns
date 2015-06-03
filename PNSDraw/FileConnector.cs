263bbeaa1ba201f12ceea021c34b52a7a899acca
kiskacsa08 <ahorvath1991@gmail.com>
2015-06-04 00:11:39 +0200

    out fájl parser megírva, limit nélkül

--- PNSDraw/FileConnector.cs
+++ PNSDraw/FileConnector.cs
@@ -221,5 +221,284 @@ namespace PNSDraw
 
             return beginEnd;
         }
+
+        public static void ParseSolution(string str, PGraph graph)
+        {
+            graph.Solutions.Clear();
+
+            int begin = 0, pos = 0, index = 0;
+
+            //---------------Maximal Structure-------------------
+            if ((begin = str.IndexOf("Maximal Structure", begin)) != -1)
+            {
+                Solution sol = new Solution(index++);
+                // Title
+                pos = str.IndexOf(":", begin);
+                sol.Title = str.Substring(begin, pos - begin);
+                // Materials
+                begin = str.IndexOf("Materials", begin);
+                begin = str.IndexOf("\n", begin);
+                begin++;
+                pos = str.IndexOf("Operating units", begin);
+                string[] mats = (str.Substring(begin, pos-begin)).Split(',');
+                foreach (string mat in mats)
+                {
+                    sol.AddMaterial(mat.Trim(), 1);
+                }
+                // Operating units
+                begin = pos;
+                begin = str.IndexOf("\n", begin);
+                begin++;
+                string[] ops;
+                if ((pos = str.IndexOf("Solution structure",begin)) != -1)
+                {
+                    ops = (str.Substring(begin, pos - begin)).Split(',');
+                }
+                else if ((pos = str.IndexOf("Feasible structure", begin)) != -1)
+                {
+                    ops = (str.Substring(begin, pos - begin)).Split(',');
+                }
+                else
+                {
+                    pos = str.IndexOf("End", begin);
+                    ops = (str.Substring(begin, pos - begin)).Split(',');
+                }
+
+                foreach (string op in ops)
+                {
+                    sol.AddOperatingUnit(op.Trim(), 1);
+                }
+                graph.Solutions.Add(sol);
+            }
+
+            //---------------SSG-----MSG---------------------------
+            if (str.IndexOf("Solution structure", 0) != -1)
+            {
+                while ((begin = str.IndexOf("Solution structure", begin)) != -1)
+                {
+                    Solution sol = new Solution(index++, "Solution structure #" + (index-1).ToString());
+                    // Materials
+                    begin = str.IndexOf("Materials", begin);
+                    begin = str.IndexOf("\n", begin);
+                    begin++;
+                    pos = str.IndexOf("Operating units", begin);
+                    string[] mats = (str.Substring(begin, pos - begin)).Split(',');
+                    foreach (string mat in mats)
+                    {
+                        sol.AddMaterial(mat.Trim(), 1);
+                    }
+                    // Operating units
+                    begin = pos;
+                    begin = str.IndexOf("\n", begin);
+                    begin++;
+                    string[] ops;
+                    if ((pos = str.IndexOf("Solution structure", begin)) != -1)
+                    {
+                        ops = (str.Substring(begin, pos - begin)).Split(',');
+                    }
+                    else
+                    {
+                        pos = str.IndexOf("End", begin);
+                        ops = (str.Substring(begin, pos - begin)).Split(',');
+                    }
+
+                    foreach (string op in ops)
+                    {
+                        sol.AddOperatingUnit(op.Trim(), 1);
+                    }
+                    graph.Solutions.Add(sol);
+                }
+            }
+            //------------------------------------------------------
+
+            //-------------------ABB---SSG+LP-----------------------
+            if (str.IndexOf("Feasible structure", 0) != -1)
+            {
+                while ((begin = str.IndexOf("Feasible structure", begin)) != -1)
+                {
+                    Solution sol = new Solution(index++, "Feasible structure #" + (index - 1).ToString());
+                    //Materials
+                    begin = str.IndexOf("Materials", begin);
+                    begin = str.IndexOf("\n", begin);
+                    begin++;
+                    pos = str.IndexOf("Operating units", begin);
+                    string[] mats = (str.Substring(begin, pos - begin)).Split('\n');
+                    foreach (string mat in mats)
+                    {
+                        string matStr = mat.Trim();
+                        if (matStr.Length == 0)
+                        {
+                            continue;
+                        }
+                        string name;
+                        double cost = 0;
+                        double rate;
+                        bool costSpecified = false;
+                        int matBegin = 0, matPos = 0;
+                        if (matStr.IndexOf("(", matBegin) != -1)
+                        {
+                            costSpecified = true;
+                            matPos = matStr.IndexOf(" ", matBegin);
+                            name = matStr.Substring(matBegin, matPos - matBegin);
+                            matBegin = matStr.IndexOf("(", matBegin);
+                            matBegin++;
+                            matPos = matStr.IndexOf(" ", matBegin);
+                            //Console.WriteLine(matStr.Substring(matBegin, matPos - matBegin));
+                            cost = Convert.ToDouble(matStr.Substring(matBegin, matPos - matBegin), CultureInfo.GetCultureInfo("en-GB"));
+                            //Console.WriteLine(cost);
+                        }
+                        else
+                        {
+                            matPos = matStr.IndexOf(":", matBegin);
+                            name = matStr.Substring(matBegin, matPos - matBegin);
+                        }
+                        //Console.WriteLine(name);
+                        matBegin = matStr.IndexOf(":", matBegin);
+                        matBegin += 2;
+                        matPos = matStr.Length;
+                        //Console.WriteLine("Hossz: " + matPos);
+                        //Console.WriteLine(matStr.Substring(matBegin, matPos - matBegin));
+                        if (matStr.Substring(matBegin, matPos - matBegin).Equals("balanced"))
+                        {
+                            rate = 0;
+                        }
+                        else
+                        {
+                            string rateStr = matStr.Substring(matBegin, matPos - matBegin);
+                            int beginRate = 0;
+                            int posRate = rateStr.IndexOf(" ", beginRate);
+                            rate = Convert.ToDouble(rateStr.Substring(beginRate, posRate - beginRate), CultureInfo.GetCultureInfo("en-GB"));
+                            //Console.WriteLine("Rate: " + rate);
+                        }
+                        //Console.WriteLine(matStr);
+                        if (costSpecified)
+                        {
+                            sol.AddMaterial(name, rate, cost);
+                        }
+                        else
+                        {
+                            sol.AddMaterial(name, rate);
+                        }
+                    }
+                    //Operating units
+                    begin = pos;
+                    begin = str.IndexOf("\n", begin);
+                    begin++;
+                    pos = str.IndexOf("Total annual cost", begin);
+                    string opStr = str.Substring(begin, pos - begin);
+                    //Console.WriteLine(opStr);
+                    string[] opsLines = opStr.Split('\n');
+                    for (int i = 0; i < opsLines.Length; i++)
+                    {
+                        if (opsLines[i].Length > 0)
+                        {
+                            string opsLine = opsLines[i].Trim();
+                            string checkChar = opsLine.Substring(0, 1);
+                            int tmp;
+                            //Console.WriteLine(opsLine);
+                            if (int.TryParse(checkChar, out tmp) == true)
+                            {
+                                string name;
+                                double size;
+                                double cost;
+                                List<MaterialProperty> inputMats = new List<MaterialProperty>();
+                                List<MaterialProperty> outputMats = new List<MaterialProperty>();
+
+                                for (int j = i+1; j < opsLines.Length; j++)
+                                {
+                                    if (opsLines[j].Length > 0)
+                                    {
+                                        checkChar = opsLines[j].Trim().Substring(0, 1);
+                                        //Console.WriteLine(checkChar);
+                                        if (int.TryParse(checkChar, out tmp) == false)
+                                        {
+                                            opsLine = opsLine + " " + opsLines[j].Trim();
+                                        }
+                                        else
+                                        {
+                                            break;
+                                        }
+                                    }
+                                }
+                                int opsBegin = 0, opsPos;
+                                opsPos = opsLine.IndexOf("*", opsBegin);
+                                size = Convert.ToDouble(opsLine.Substring(opsBegin, opsPos - opsBegin), CultureInfo.GetCultureInfo("en-GB"));
+                                //Console.WriteLine(size);
+                                opsBegin = opsPos + 1;
+                                opsPos = opsLine.IndexOf(" ", opsBegin);
+                                name = opsLine.Substring(opsBegin, opsPos - opsBegin);
+                                //Console.WriteLine(name);
+                                opsBegin = opsLine.IndexOf("(", opsPos);
+                                opsBegin++;
+                                opsPos = opsLine.IndexOf(" ", opsBegin);
+                                cost = Convert.ToDouble(opsLine.Substring(opsBegin, opsPos - opsBegin), CultureInfo.GetCultureInfo("en-GB"));
+                                //Console.WriteLine(cost);
+                                opsBegin = opsLine.IndexOf(":", opsPos);
+                                opsPos = opsLine.IndexOf("=>", opsBegin);
+                                string opsInMats = opsLine.Substring(opsBegin, opsPos - opsBegin).Trim();
+                                //Console.WriteLine(opsInMats);
+                                if (opsInMats.Length > 1)
+                                {
+                                    int matBegin;
+                                    int matPos = 0;
+                                    int count = opsInMats.Split('(').Length - 1;
+                                    for (int j = 0; j < count; j++)
+                                    {
+                                        matBegin = matPos + 2;
+                                        matPos = opsInMats.IndexOf("(", matBegin) - 1;
+                                        string matName = opsInMats.Substring(matBegin, matPos - matBegin);
+                                        matBegin = matPos + 2;
+                                        matPos = opsInMats.IndexOf(" ", matBegin);
+                                        //Console.WriteLine(opsInMats.Substring(matBegin, matPos - matBegin));
+                                        double matCost = Convert.ToDouble(opsInMats.Substring(matBegin, matPos - matBegin), CultureInfo.GetCultureInfo("en-GB"));
+                                        //Console.WriteLine(matCost);
+                                        matPos = opsInMats.IndexOf(")", matPos);
+                                        inputMats.Add(new MaterialProperty(matName, matCost, 0));
+                                    }
+                                }
+                                //Console.WriteLine(opsLine);
+                                opsBegin = opsPos;
+                                opsPos = opsLine.Length;
+                                //Console.WriteLine("opsBegin: " + opsBegin + ", opsPos: " + opsPos);
+                                string opsOutMats = opsLine.Substring(opsBegin, opsPos - opsBegin).Trim();
+                                //Console.WriteLine(opsOutMats);
+                                if (opsOutMats.Length > 2)
+                                {
+                                    int matBegin;
+                                    int matPos = 0;
+                                    int count = opsOutMats.Split('(').Length - 1;
+                                    //Console.WriteLine(count);
+                                    for (int j = 0; j < count; j++)
+                                    {
+                                        matBegin = matPos + 2;
+                                        matPos = opsOutMats.IndexOf("(", matBegin) - 1;
+                                        string matName = opsOutMats.Substring(matBegin, matPos - matBegin).Trim();
+                                        matBegin = matPos + 2;
+                                        matPos = opsOutMats.IndexOf(" ", matBegin);
+                                        //Console.WriteLine(matName);
+                                        double matCost = Convert.ToDouble(opsOutMats.Substring(matBegin, matPos - matBegin), CultureInfo.GetCultureInfo("en-GB"));
+                                        //Console.WriteLine(matCost);
+                                        matPos = opsOutMats.IndexOf(")", matPos);
+                                        outputMats.Add(new MaterialProperty(matName, matCost, 0));
+                                    }
+                                }
+                                sol.AddOperatingUnit(name, size, cost, inputMats, outputMats);
+                            }
+                        }
+                        
+                    }
+                    // Total annual cost
+                    begin = pos;
+                    begin = str.IndexOf("= ", begin);
+                    begin = begin + 2;
+                    pos = str.IndexOf(" ", begin);
+                    double totalCost = Convert.ToDouble(str.Substring(begin, pos - begin), CultureInfo.GetCultureInfo("en-GB"));
+                    sol.OptimalValue = totalCost;
+
+                    graph.Solutions.Add(sol);
+                }
+            }
+            
+        }
     }
 }
