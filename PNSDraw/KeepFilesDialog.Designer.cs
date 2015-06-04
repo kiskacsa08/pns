78bcf35adf671d37dc835023d594f2ba5808ed12
kiskacsa08 <ahorvath1991@gmail.com>
2015-06-04 15:51:26 +0200

    delete

--- PNSDraw/KeepFilesDialog.Designer.cs
+++ PNSDraw/KeepFilesDialog.Designer.cs
@@ -41,9 +41,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(297, 20);
+            this.label1.Size = new System.Drawing.Size(306, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Do you want to keep the temporary files?";
+            this.label1.Text = "Do you want to delete the temporary files?";
             // 
             // lblInFileName
             // 
