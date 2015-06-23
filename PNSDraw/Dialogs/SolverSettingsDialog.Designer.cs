47d66556da3d02908b4cb76f9ac690617a9234fb
kiskacsa08 <ahorvath1991@gmail.com>
2015-06-23 21:03:13 +0200

    SolverSettings

---
+++ PNSDraw/Dialogs/SolverSettingsDialog.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace PNSDraw.Dialogs
+{
+    partial class SolverSettingsDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numLimit = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.numProcesses = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chkOnline = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numPort = new System.Windows.Forms.NumericUpDown();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numLimit)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numProcesses)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.numPort);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.txtHost);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.chkOnline);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.btnBrowse);
+            this.panel1.Controls.Add(this.txtFolder);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.numProcesses);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.numLimit);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(628, 453);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Number of processes";
+            // 
+            // numLimit
+            // 
+            this.numLimit.Location = new System.Drawing.Point(205, 53);
+            this.numLimit.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.numLimit.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numLimit.Name = "numLimit";
+            this.numLimit.Size = new System.Drawing.Size(120, 20);
+            this.numLimit.TabIndex = 1;
+            this.numLimit.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numLimit.ValueChanged += new System.EventHandler(this.numLimit_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Solutions limit";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnCancel);
+            this.panel2.Controls.Add(this.btnOk);
+            this.panel2.Controls.Add(this.btnApply);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 453);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(628, 60);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(536, 25);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnOk
+            // 
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.Location = new System.Drawing.Point(455, 25);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 4;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // btnApply
+            // 
+            this.btnApply.Enabled = false;
+            this.btnApply.Location = new System.Drawing.Point(358, 25);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(75, 23);
+            this.btnApply.TabIndex = 3;
+            this.btnApply.Text = "Apply";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // numProcesses
+            // 
+            this.numProcesses.Location = new System.Drawing.Point(205, 104);
+            this.numProcesses.Maximum = new decimal(new int[] {
+            64,
+            0,
+            0,
+            0});
+            this.numProcesses.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numProcesses.Name = "numProcesses";
+            this.numProcesses.Size = new System.Drawing.Size(120, 20);
+            this.numProcesses.TabIndex = 3;
+            this.numProcesses.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numProcesses.ValueChanged += new System.EventHandler(this.numLimit_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Temporary files folder";
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(205, 156);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.ReadOnly = true;
+            this.txtFolder.Size = new System.Drawing.Size(296, 20);
+            this.txtFolder.TabIndex = 5;
+            this.txtFolder.TextChanged += new System.EventHandler(this.numLimit_ValueChanged);
+            this.txtFolder.DoubleClick += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("Stencil", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Location = new System.Drawing.Point(507, 156);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(29, 22);
+            this.btnBrowse.TabIndex = 6;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Use online solver";
+            // 
+            // chkOnline
+            // 
+            this.chkOnline.AutoSize = true;
+            this.chkOnline.Location = new System.Drawing.Point(205, 211);
+            this.chkOnline.Name = "chkOnline";
+            this.chkOnline.Size = new System.Drawing.Size(15, 14);
+            this.chkOnline.TabIndex = 8;
+            this.chkOnline.UseVisualStyleBackColor = true;
+            this.chkOnline.CheckedChanged += new System.EventHandler(this.chkOnline_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(32, 263);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Online solver host";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(205, 260);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(296, 20);
+            this.txtHost.TabIndex = 10;
+            this.txtHost.TextChanged += new System.EventHandler(this.numLimit_ValueChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(32, 318);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Online solver port";
+            // 
+            // numPort
+            // 
+            this.numPort.Location = new System.Drawing.Point(205, 316);
+            this.numPort.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numPort.Minimum = new decimal(new int[] {
+            1025,
+            0,
+            0,
+            0});
+            this.numPort.Name = "numPort";
+            this.numPort.Size = new System.Drawing.Size(120, 20);
+            this.numPort.TabIndex = 12;
+            this.numPort.Value = new decimal(new int[] {
+            1025,
+            0,
+            0,
+            0});
+            this.numPort.ValueChanged += new System.EventHandler(this.numLimit_ValueChanged);
+            // 
+            // SolverSettingsDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 513);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "SolverSettingsDialog";
+            this.Text = "SolverSettingsDialog";
+            this.Load += new System.EventHandler(this.SolverSettingsDialog_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numLimit)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numProcesses)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numLimit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numProcesses;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.CheckBox chkOnline;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numPort;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Label label5;
+    }
+}
